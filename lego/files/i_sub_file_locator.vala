robinhellen/vldraw

commit 9c24f391b26c674fc8686f6f41a914bf60d6e65a

start extracting more interfaces from LdrawLibrary.

lego/files/i_sub_file_locator.vala
@@ -12,16 +12,21 @@ namespace Ldraw.Lego
 
 	public class LibrarySubFileLocator : Object, ISubFileLocator
 	{
-		private LdrawLibrary library;
+		private IDatFileCache library;
 
-		public LibrarySubFileLocator(LdrawLibrary library)
+		public LibrarySubFileLocator(IDatFileCache library)
 		{
 			this.library = library;
 		}
 
 		public LdrawObject GetObjectFromReference(string reference)
+			throws ParseError
 		{
-			return library.GetPartByName(reference.substring(0, reference.last_index_of(".")));
+			LdrawPart part;
+			if(library.TryGetPart(reference, out part))
+				return part.MainObject;
+
+			throw new ParseError.MissingFile(@"No such object '$reference'.");
 		}
 	}
 

lego/library/i_library.vala
@@ -10,4 +10,28 @@ namespace Ldraw.Lego.Library
 
 		public abstract Collection<LdrawPart> GetVariantsOf(LdrawPart part);
 	}
+
+	public interface IDatFileCache : Object
+	{
+		public abstract bool TryGetPrimitive(string name, out LdrawPrimitive primitive);
+
+		public abstract bool TryGetHiresPrimitive(string name, out LdrawHiresPrimitive primitive);
+
+		public abstract bool TryGetPart(string name, out LdrawPart part);
+
+		public abstract bool TryGetSubPart(string name, out LdrawSubPart part);
+	}
+
+	public interface ILdrawFolders
+	{
+		public abstract File LibraryDirectory { get; }
+
+		public abstract File PartsDirectory { get; }
+
+		public abstract File SubPartsDirectory { get; }
+
+		public abstract File PrimitivesDirectory { get; }
+
+		public abstract File HiresPrimitivesDirectory { get; }
+	}
 }

lego/library/ldraw_library.vala
@@ -4,7 +4,7 @@ using Ldraw;
 
 namespace Ldraw.Lego.Library
 {
-	public class LdrawLibrary : Object, ILibrary
+	public class LdrawLibrary : Object, ILibrary, ILdrawFolders, IDatFileCache
 	{
 		private File m_LibraryDir;
 		private File m_PartsDirectory;

commit 3220fed16d7bf26fb31c704f14869a01ea1cb017

fix the bug introduced in 9c24f391b26c674fc8686f6f41a914bf60d6e65a.

lego/files/i_sub_file_locator.vala
@@ -6,7 +6,7 @@ namespace Ldraw.Lego
 {
 	public interface ISubFileLocator : Object
 	{
-		public abstract LdrawObject GetObjectFromReference(string reference)
+		public abstract LdrawObject? GetObjectFromReference(string reference)
 			throws ParseError;
 	}
 
@@ -19,14 +19,16 @@ namespace Ldraw.Lego
 			this.library = library;
 		}
 
-		public LdrawObject GetObjectFromReference(string reference)
+		public LdrawObject? GetObjectFromReference(string reference)
 			throws ParseError
 		{
+			var partName = reference.substring(0, reference.last_index_of("."));
+
 			LdrawPart part;
-			if(library.TryGetPart(reference, out part))
+			if(library.TryGetPart(partName, out part))
 				return part.MainObject;
 
-			throw new ParseError.MissingFile(@"No such object '$reference'.");
+			return null;
 		}
 	}
 
@@ -41,7 +43,7 @@ namespace Ldraw.Lego
 			m_Proxies = new ArrayList<ProxyLdrawObject>();
 		}
 
-		public LdrawObject GetObjectFromReference(string reference)
+		public LdrawObject? GetObjectFromReference(string reference)
 			throws ParseError
 		{
 			var baseVal = m_Locator.GetObjectFromReference(reference);
@@ -97,7 +99,7 @@ namespace Ldraw.Lego
 			this.library = library;
 		}
 
-		public LdrawObject GetObjectFromReference(string reference)
+		public LdrawObject? GetObjectFromReference(string reference)
 			throws ParseError
 		{
 			string[] toks = reference.split_set("/\\");