8d3e8de60de98154d93f3875ac151fa435176439
Robin Hellen <robin.hellen@cantab.net>
2018-03-04 17:09:41 +0000

    Structure for building on windows (doesn\'t run yet)

---
+++ install_prerequisites.sh
@@ -0,0 +1,9 @@
+   
+pacman -S mingw-w64-x86_64-gcc
+pacman -S mingw-w64-x86_64-pkg-config
+pacman -S mingw-w64-x86_64-vala
+pacman -S make
+pacman -S mingw-w64-x86_64-libgee
+pacman -S mingw-w64-x86_64-gtk3
+pacman -S mingw-w64-x86_64-glew
+
